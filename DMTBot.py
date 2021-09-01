09d67433dd862bcb3047d686ba0e9dac166ccf12
D <68999013+OwnerCods@users.noreply.github.com>
2021-09-01 14:41:12 +0700

    Update DMTBot.py

--- DMTBot.py
+++ DMTBot.py
@@ -98,7 +98,7 @@ def GetTronBalance(wallet):
 def GetTonBalance(wallet):
     walletBalance = str(wallet.Information.json()['result'])
     if walletBalance != '0':
-        walletBalance = ToCorrectView(walletBalance, 18)
+        walletBalance = ToCorrectView(walletBalance, 9)
     return walletBalance
 
 def GetBnbBalance(wallet):
