b177379dd25575e09669cc17f3124a795bb5db3a
D <68999013+OwnerCods@users.noreply.github.com>
2021-09-01 19:04:15 +0700

    Update DMTBot.py

--- DMTBot.py
+++ DMTBot.py
@@ -226,10 +226,12 @@ def SetWalletBalance(message):
             
     elif (OurWallet.Currency == 'Ton(TON)'):
          OurWallet.SetInformation(GetTonInformation(OurWallet.Address))
-         OurWallet.SetStatus(OurWallet.Information.json()['ok']['true'])
-         if str(OurWallet.Information.json()['ok']['true']):
-                OurWallet.Status = '1'
-                OurWallet.SetBalance(GetTonBalance(OurWallet))           
+         try:
+             if str(OurWallet.Information.json()['"ok":false']):
+                OurWallet.Status = '0'
+         except:
+             OurWallet.Status = '1'       
+             OurWallet.SetBalance(GetTonBalance(OurWallet))           
     
     elif (OurWallet.Currency == 'Tether(BEP20)'):
         OurWallet.SetInformation(GetUsdtbnbInformation(OurWallet.Address))
