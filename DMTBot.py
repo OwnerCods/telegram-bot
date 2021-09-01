5ac92534ad5c20122d1176066dd4bf1819ec1eaf
D <68999013+OwnerCods@users.noreply.github.com>
2021-09-01 15:23:59 +0700

    Update DMTBot.py

--- DMTBot.py
+++ DMTBot.py
@@ -227,7 +227,7 @@ def SetWalletBalance(message):
     elif (OurWallet.Currency == 'Ton(TON)'):
          OurWallet.SetInformation(GetTonInformation(OurWallet.Address))
          try:
-             if str(OurWallet.Information.json()[]['false']):   
+             if str(OurWallet.Information.json()[':false']):   
                  OurWallet.Status = '0'
          except:
              OurWallet.Status = '1'
